db68d093afc0e4622b850fa2a27713d76066cba8
evann delaunay <evann@wixar.io>
2022-08-29 10:39:00 +0200

    Started working on ImmersiveHUD

---
+++ ImmersiveHUD/Main.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+using ABI_RC.Core.UI;
+
+[assembly: MelonInfo(typeof(ImmersiveHUD.Main), "ImmersiveHUD", "1.0.0", "Ikeiwa")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+namespace ImmersiveHUD
+{
+    public class Main : MelonMod
+    {
+        public static Main instance;
+
+        public static MelonPreferences_Category settingsCategory;
+        public static MelonPreferences_Entry<int> hideDelay;
+        public static MelonPreferences_Entry<bool> showOnMicOn;
+        public static MelonPreferences_Entry<bool> showOnMicOff;
+        public static MelonPreferences_Entry<bool> showOnFriendRequest;
+        public static MelonPreferences_Entry<bool> showOnInvite;
+        public static MelonPreferences_Entry<bool> showOnMessage;
+
+        private object currentHideTimer;
+        private bool forceShow;
+
+        public override void OnApplicationStart()
+        {
+            if(instance == null)
+                instance = this;
+
+            settingsCategory = MelonPreferences.CreateCategory("ImmersiveHUD");
+            hideDelay = settingsCategory.CreateEntry("Hide delay (seconds)", 10);
+            showOnMicOn = settingsCategory.CreateEntry("Show on unmute", true);
+            showOnMicOff = settingsCategory.CreateEntry("Show on mute", true);
+            showOnFriendRequest = settingsCategory.CreateEntry("Show on friend request", true);
+            showOnInvite = settingsCategory.CreateEntry("Show on invite", true);
+            showOnMessage = settingsCategory.CreateEntry("Show on system message", true);
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if(buildIndex == 3)
+                StartHideTimer();
+        }
+
+        public void ForceShowHUD()
+        {
+            forceShow = true;
+            CohtmlHud.Instance.RestoreHud();
+        }
+
+        public void StartHideTimer(bool force = false)
+        {
+            if(force) forceShow = false;
+            else if(forceShow) return;
+
+            if(currentHideTimer != null)
+                MelonCoroutines.Stop(currentHideTimer);
+            currentHideTimer = MelonCoroutines.Start(HideAfterDelay(hideDelay.Value));
+        }
+
+        private IEnumerator HideAfterDelay(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            CohtmlHud.Instance.HideHud();
+        }
+    }
+}
---
+++ ImmersiveHUD/Patches.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.UI;
+using HarmonyLib;
+
+namespace ImmersiveHUD
+{
+    [HarmonyPatch(typeof(CohtmlHud), "UpdateNotifierStatus")]
+    class Patch_CohtmlHud_UpdateNotifierStatus
+    {
+        private static int lastFriendCount = 0;
+        private static int lastInviteCount = 0;
+
+        public static void Postfix()
+        {
+            if (Main.showOnFriendRequest.Value && ViewManager.Instance.FriendRequests.Count > lastFriendCount)
+                Main.instance.StartHideTimer();
+            else if (Main.showOnInvite.Value && ViewManager.Instance.Invites.Count > lastInviteCount)
+                Main.instance.StartHideTimer();
+
+            lastFriendCount = ViewManager.Instance.FriendRequests.Count;
+            lastInviteCount = ViewManager.Instance.Invites.Count;
+        }
+    }
+
+    [HarmonyPatch(typeof(CohtmlHud), nameof(CohtmlHud.UpdateMicStatus))]
+    class Patch_CohtmlHud_UpdateMicStatus
+    {
+        public static void Postfix(bool active)
+        {
+            if(Main.showOnMicOn.Value && active)
+                Main.instance.StartHideTimer();
+            else if(Main.showOnMicOff.Value && !active)
+                Main.instance.StartHideTimer();
+        }
+    }
+
+    [HarmonyPatch(typeof(CohtmlHud), nameof(CohtmlHud.SelectPropToSpawn))]
+    class Patch_CohtmlHud_SelectPropToSpawn
+    {
+        public static void Postfix()
+        {
+            Main.instance.ForceShowHUD();
+        }
+    }
+
+    [HarmonyPatch(typeof(CohtmlHud), nameof(CohtmlHud.ClearPropToSpawn))]
+    class Patch_CohtmlHud_ClearPropToSpawn
+    {
+        public static void Postfix()
+        {
+            Main.instance.StartHideTimer(true);
+        }
+    }
+
+    [HarmonyPatch(typeof(CohtmlHud), nameof(CohtmlHud.ViewDropText), new Type[] { typeof(string), typeof(string) })]
+    class Patch_CohtmlHud_ViewDropText_1
+    {
+        public static void Postfix()
+        {
+            if(Main.showOnMessage.Value)
+                Main.instance.StartHideTimer();
+        }
+    }
+
+    [HarmonyPatch(typeof(CohtmlHud), nameof(CohtmlHud.ViewDropText), new Type[] { typeof(string), typeof(string), typeof(string) })]
+    class Patch_CohtmlHud_ViewDropText_2
+    {
+        public static void Postfix()
+        {
+            if(Main.showOnMessage.Value)
+                Main.instance.StartHideTimer();
+        }
+    }
+
+    [HarmonyPatch(typeof(CohtmlHud), nameof(CohtmlHud.ViewDropTextImmediate), new Type[] { typeof(string), typeof(string), typeof(string) })]
+    class Patch_CohtmlHud_ViewDropTextImmediate
+    {
+        public static void Postfix()
+        {
+            if(Main.showOnMessage.Value)
+                Main.instance.StartHideTimer();
+        }
+    }
+}
